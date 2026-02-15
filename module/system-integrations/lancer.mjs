e5b61f208b88292e13c83ba9ffa44aeed74841c3
Will Bennion <wibble199@users.noreply.github.com>
2026-02-15 11:40:50 +0000

    Add max weapon range Lancer radius extension

--- module/system-integrations/lancer.mjs
+++ module/system-integrations/lancer.mjs
@@ -1,11 +1,11 @@
 import { registerRadiusExpressionExtension } from "../data/aura-radius-expression-extensions.mjs";
 
-const weaponDefaultRange = 1; // "Unless specified otherwise, all weapons default to 1 Threat"
+const weaponDefaultThreat = 1; // "Unless specified otherwise, all weapons default to 1 Threat"
 
 export function setupLancerSystemIntegration() {
-	// Register a radius expression extension that resolves to the threat range of an actor
+	// Register a radius expression extension that resolves to the largest threat of an actor
 	registerRadiusExpressionExtension(
-		"lancer.actor_threat",
+		"lancer.actor_max_threat",
 		actor => actor?.items?.reduce((maxRange, item) => {
 			// Ignore destroyed items
 			if (item.system.destroyed)
@@ -13,28 +13,62 @@ export function setupLancerSystemIntegration() {
 
 			switch (item.type) {
 				case "mech_weapon":
-					return Math.max(maxRange, findThreatRange(item.system.active_profile.range));
+					return Math.max(maxRange, findRange(item.system.active_profile.range, "Threat", weaponDefaultThreat));
 
 				case "npc_feature":
 					return item.system.type === "Weapon" // Ignore non-weapon NPC features
-						? Math.max(maxRange, findThreatRange(item.system.range))
+						? Math.max(maxRange, findRange(item.system.range, "Threat", weaponDefaultThreat))
 						: maxRange;
 
 				case "pilot_weapon":
-					return Math.max(maxRange, findThreatRange(item.system.range));
+					return Math.max(maxRange, findRange(item.system.range, "Threat", weaponDefaultThreat));
 
 				default:
 					return maxRange;
 			}
 		}, -1) ?? -1,
 		{
-			description: "The largest/maximum threat range for the actor based on it's items. Returns -1 (which will disable the aura) if no items grant threat."
+			description: "The largest/maximum weapon threat for the actor based on it's items. Returns -1 (which will disable the aura) if no items grant threat."
+		}
+	);
+
+	// Register a radius expression extension that resolves to the largest range of an actor
+	registerRadiusExpressionExtension(
+		"lancer.actor_max_range",
+		actor => actor?.items?.reduce((maxRange, item) => {
+			// Ignore destroyed items
+			if (item.system.destroyed)
+				return maxRange;
+
+			switch (item.type) {
+				case "mech_weapon":
+					return Math.max(maxRange, findRange(item.system.active_profile.range, "Range"));
+
+				case "npc_feature":
+					return item.system.type === "Weapon" // Ignore non-weapon NPC features
+						? Math.max(maxRange, findRange(item.system.range, "Range"))
+						: maxRange;
+
+				case "pilot_weapon":
+					return Math.max(maxRange, findRange(item.system.range, "Range"));
+
+				default:
+					return maxRange;
+			}
+		}, -1) ?? -1,
+		{
+			description: "The largest/maximum weapon range for the actor based on it's items. Returns -1 (which will disable the aura) if no items have a range."
 		}
 	);
 }
 
-/** Attempts to find a threat value from the given range array. */
-function findThreatRange(ranges) {
-	const threat = +ranges?.find(r => r.type === "Threat")?.val;
-	return Number.isNaN(threat) ? weaponDefaultRange : threat;
+/**
+ * Attempts to find a range value from the given range array.
+ * @param {{ type: string; val: number; }[]} ranges
+ * @param {string} rangeType
+ * @param {number} defaultRange Default range if the specific rangeType could not be found
+ */
+function findRange(ranges, rangeType, defaultRange = -1) {
+	const threat = +ranges?.find(r => r.type === rangeType)?.val;
+	return Number.isNaN(threat) ? defaultRange : threat;
 }
